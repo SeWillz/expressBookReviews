d884195429ab5646598fe46119c4e86559d76110
SeWillz <138167827+SeWillz@users.noreply.github.com>
2023-08-17 12:26:48 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,13 +12,23 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 
 app.use("/customer/auth/*", function auth(req, res, next) {
   // Call the authentication mechanism from auth.js
-  if (auth.isAuthenticatedUser(req.body.username, req.body.password)) {
-    req.isAuthenticated = true;
-  } else {
-    req.isAuthenticated = false;
+  app.post("/login", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
   }
-  next();
-});
+ if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }});
 
 const PORT = 5000;
 
