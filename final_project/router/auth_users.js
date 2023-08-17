e2e3cb32e801f3b77fab4650af15ce369b929860
SeWillz <138167827+SeWillz@users.noreply.github.com>
2023-08-17 15:08:11 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,24 +6,70 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
-
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+    for (var user in users) {
+      if (user["username"]===username) {
+          return true;
+      }
+  }
+    return false;
+  }
+  
+  const authenticatedUser = (username,password)=>{
+      let validUser = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validUser.length > 0){
+        return true;
+      } else {
+        return false;
+      }
+    }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
+      req.session.authorization = {accessToken, username}
+      return res.status(200).json({message: "User successfully logged in."})
+    }
+    else {
+      return res.status(300).json({message: "Username and password do not match our records. Please try again."});
+    }
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+
+    if (!books[isbn]) {
+        return res.status(404).send("Unable to find this ISBN!");
+    }
+
+    delete books[isbn]['reviews'];
+
+    return res.status(200).send(`The review for the book with ISBN ${isbn} has been deleted.`);
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
